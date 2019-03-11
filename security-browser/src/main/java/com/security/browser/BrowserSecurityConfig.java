5396253fc1c2f9d1fc979c67724ad56f5162c776
Wangll-h <1196334239@qq.com>
2019-03-11 20:58:24 +0800

    使用固定用户名密码

--- security-browser/src/main/java/com/security/browser/BrowserSecurityConfig.java
+++ security-browser/src/main/java/com/security/browser/BrowserSecurityConfig.java
@@ -2,19 +2,38 @@ package com.security.browser;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		 http.httpBasic()
+		 http.formLogin()
 		     .and()
 		     .authorizeRequests()
 		     .anyRequest()
 		     .authenticated();
 	}
+	
+	 @Bean
+	 public PasswordEncoder passwordEncoder() { 
+		 return new BCryptPasswordEncoder(); 
+		 } 
+	 
+	 
+	 @Override
+	 protected void configure(AuthenticationManagerBuilder auth) throws Exception { 
+		 auth.inMemoryAuthentication() .passwordEncoder(passwordEncoder()) 
+		     .withUser("qin").password(passwordEncoder().encode("111"))
+		     .roles("USER") .and() .withUser("kai").password(passwordEncoder()
+		     .encode("222")).roles("USER"); 
+	}
+	
 }
